4429bd214ce3bfe611d5b6a93afda74be3d415ca
Marwan <marwan.00181941@gmail.com>
2026-06-21 16:41:22 +0300

    feat: integrate device preview dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:reminder_app/app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(DevicePreview(builder: (context) => const App()));
 }
