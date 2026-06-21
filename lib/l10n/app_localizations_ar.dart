d9179248300b0fe929322fe494eae27cc26330e6
Marwan <marwan.00181941@gmail.com>
2026-06-21 17:04:51 +0300

    feat: add localization support for Arabic, English, and Persian languages

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get appName => 'کاتیاد';
+
+  @override
+  String get welcome => 'اهلا و سهلا';
+
+  @override
+  String get hell => 'مرحبا';
+}
---
+++ lib/l10n/app_localizations_fa.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Persian (`fa`).
+class AppLocalizationsFa extends AppLocalizations {
+  AppLocalizationsFa([String locale = 'fa']) : super(locale);
+
+  @override
+  String get appName => 'کاتیاد';
+
+  @override
+  String get welcome => 'بەخێربێیت';
+
+  @override
+  String get hell => 'سڵاو';
+}
