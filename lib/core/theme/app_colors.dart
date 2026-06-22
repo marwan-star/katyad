60ead238c410fa4aa8de95434d6e547fe353c53b
Marwan <marwan.00181941@gmail.com>
2026-06-22 23:05:14 +0300

    refactor: added more color

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -4,4 +4,7 @@ class AppColors {
   static const lightWhite = Color(0xffFBFBFB);
   static const darkOrange = Color(0xffFFB22C);
   static const black = Color(0xff000000);
+  static const darkGreen = Color(0xff3D8D7A);
+  static const purple = Color(0xff723EC3);
+  static const lightGrey = Color(0xffBFC9D1);
 }
